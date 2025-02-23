7c62f9c81caebf18dd7d7498bf0f57be986b78e6
adarsh <adarshnagar@ucsb.edu>
2025-02-23 14:32:43 -0800

    a

--- csv_manipulation.py
+++ csv_manipulation.py
@@ -9,6 +9,8 @@ import os
 api_url = api_url = "https://data.sfgov.org/resource/wg3w-h783.csv?$limit=1000&$order=Incident_Datetime%20desc"
 new_data = pd.read_csv(api_url)
 
+
+# Identify the local file in your CD
 local_file = "shortenedSFcrimedata2_modified.csv"
 local_file = os.path.expanduser(local_file)
 
@@ -17,8 +19,10 @@ if os.path.exists(local_file):
 else:
     local_data = pd.DataFrame()
 
+#Update Existing CSV with new, updated values
 df = pd.concat([local_data, new_data], ignore_index=True)
 
+# Identification of Unique Values, to measure and cutoff repeated data
 unique_col = "Incident_ID"  
 if unique_col in df.columns:
     df = df.drop_duplicates(subset=[unique_col])
@@ -26,7 +30,7 @@ else:
     df = df.drop_duplicates()
 
 
-
+# Weight Dictionary for each Crime
 weights = {
     "Arson": 70,
     "Assault": 85,
@@ -51,21 +55,25 @@ weights = {
     "Stolen Property": 45
 }
 
+# Crime Weight Column Creation
 df["Incident_Weight"] = df["Incident Category"].map(weights)
 
+#Convertion of "Incident Datetime" to Datetime format
 df["Incident Datetime"] = pd.to_datetime(df["Incident Datetime"], errors='coerce')
 
 
 
 
-
+# Initializing Variables for Exponential Scoring
 current_date = datetime.now()
 gamma = 0.01
 emvec = []
 
+# Creation of Exponential Score Column
 df["Exponential_Score"] = df["Incident_Weight"] * np.exp((-1) * gamma * (current_date - df["Incident Datetime"]).dt.days)
 df = df.iloc[:, :37]
 
+# Function Summing the Crime Rates of Each District, Returning Crime Rate of the Districts over the Entire City (% from 0 to 1)
 def district_sum(dataframe):
     districts_crime_rates = dataframe.groupby("Police District")["Exponential_Score"].sum()
     city_crime_rate = districts_crime_rates.sum()
@@ -73,9 +81,39 @@ def district_sum(dataframe):
     return districts_over_city
 
 
+
+def district_risk(dsv):
+    pmin = min(dsv.values())
+    pmax = max(dsv.values())
+    polar_diff = pmax-pmin
+    if polar_diff == 0:
+        return {district: 0 for district in dsv}
+
+    alpha = 1.5
+    power_curved_vec = {
+        district: np.power(((score - pmin) / polar_diff), alpha)
+        for district, score in dsv.items()}
+    return power_curved_vec
+
+
+
+
+
+
+
+# Sort dates in the file from newest to oldest
 df = df.sort_values(by=df.columns[0], ascending=False)
 
 
+# Exporting 2 Files
+
+
+# Main File, Condensed
 output_file_path = "~/Desktop/shortenedSFcrimedata2_modified.csv"
 df.to_csv(output_file_path, index=False)
+
+# Power Curved Vals
+risk_scores = district_risk(district_sum(df))
+pcdf = pd.DataFrame(risk_scores.items(), columns=["District", "Score"])
+pcdf.to_csv("power_curved_values.csv", index=False)
 
