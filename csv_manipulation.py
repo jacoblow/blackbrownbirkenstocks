7a260175e5822e9aa002e0cf4ff697d58f1fe029
adarsh <adarshnagar@ucsb.edu>
2025-02-22 23:36:39 -0800

    a

--- csv_manipulation.py
+++ csv_manipulation.py
@@ -3,11 +3,30 @@ import folium
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import os
+
+
+api_url = api_url = "https://data.sfgov.org/resource/wg3w-h783.csv?$limit=1000&$order=Incident_Datetime%20desc"
+new_data = pd.read_csv(api_url)
+
+local_file = "~/Desktop/shortenedSFcrimedata2_modified.csv"
+local_file = os.path.expanduser(local_file)
+
+if os.path.exists(local_file):
+    local_data = pd.read_csv(local_file)
+else:
+    local_data = pd.DataFrame()
+
+df = pd.concat([local_data, new_data], ignore_index=True)
+
+unique_col = "Incident_ID"  
+if unique_col in df.columns:
+    df = df.drop_duplicates(subset=[unique_col])
+else:
+    df = df.drop_duplicates()
 
 
 
-file_path = "~/Desktop/shortenedSFcrimedata2.csv"
-df = pd.read_csv(file_path)
 weights = {
     "Arson": 40,
     "Assault": 90,
@@ -42,8 +61,7 @@ gamma = 0.001
 emvec = []
 
 df["Exponential_Score"] = df["Incident_Weight"] * np.exp((-1) * gamma * (current_date - df["Incident Datetime"]).dt.days)
-
-print(df["Exponential_Score"])
+df = df.iloc[:, :37]
 
 output_file_path = "~/Desktop/shortenedSFcrimedata2_modified.csv"
 df.to_csv(output_file_path, index=False)
